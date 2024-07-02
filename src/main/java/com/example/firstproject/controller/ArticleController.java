f9252893e6d1f646984575900acd2599178c677f
unknown <leegj420@gmail.com>
2024-07-02 17:39:12 +0900

    7.3.4 수정 데이터 받아 오기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -88,5 +88,13 @@ public class ArticleController {
         // 3. 뷰 페이지 설정하기
         return "articles/edit";
     }
+
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form){
+
+        log.info(form.toString());
+        return "";
+    }
     
 }
