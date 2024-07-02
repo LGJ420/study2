196f432e96320608ebdb28c079df10d44b5b83a8
unknown <leegj420@gmail.com>
2024-07-02 10:39:50 +0900

    3.2.4 폼 데이터 받기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
@@ -12,4 +13,10 @@ public class ArticleController {
         return "articles/new";
     }
     
+
+    @PostMapping("articles/create")
+    public String createArticle() {
+
+        return "";
+    }
 }
