5b1da72ffe135469bfe9e2a7ed133b2e899bec31
unknown <leegj420@gmail.com>
2024-07-02 10:48:37 +0900

    3.2.6 폼 데이터를 DTO에 담기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -4,7 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.firstproject.dto.ArticleForm;
+
+import lombok.extern.log4j.Log4j2;
+
 @Controller
+@Log4j2
 public class ArticleController {
     
     @GetMapping("/articles/new")
@@ -15,8 +20,9 @@ public class ArticleController {
     
 
     @PostMapping("articles/create")
-    public String createArticle() {
+    public String createArticle(ArticleForm form) {
 
+        log.info(form.toString());
         return "";
     }
 }
