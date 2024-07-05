8407b9f5a81f741ae2c806e44002ff77d5904d0f
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-07-05 20:04:39 +0900

    15.2 댓글 컨트롤러와 서비스 틀 만들기

---
+++ src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -0,0 +1,27 @@
+package com.example.firstproject.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.firstproject.service.CommentService;
+
+@RestController
+public class CommentApiController {
+    
+    @Autowired
+    private CommentService commentService;
+
+    // 1. 댓글 조회
+
+
+
+    // 2. 댓글 생성
+
+
+
+    // 3. 댓글 수정
+
+
+
+    // 4. 댓글 삭제
+}
