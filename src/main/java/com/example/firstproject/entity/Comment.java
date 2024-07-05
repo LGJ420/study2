6d47cbb553eda5ec364d0885019e6b32a67ba5ea
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-07-05 14:28:01 +0900

    14.2.1 댓글 엔티티 만들기

---
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -0,0 +1,35 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String body;
+}
